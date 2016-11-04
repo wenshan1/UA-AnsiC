cc6aae48dc2e2534077de97e46aab02c1932ab93
Randy Armstrong <randy@ravensjade.com>
2016-11-03 19:46:23 -0700

    Fix OAuth2 samples.

--- prototypes/oauth2/build_outh2.bat
+++ prototypes/oauth2/build_outh2.bat
@@ -1,6 +1,6 @@
 @ECHO off
 REM ****************************************************************************************************************
-REM ** This script builds Azure-uAMQP-C for use by the ANSI C samples.
+REM ** This script builds the ANSI-C OAuth2 samples.
 REM ** This requires CMAKE to be installed.
 REM ** This must be run from a Visual Studio command line.
 REM ****************************************************************************************************************
@@ -18,12 +18,12 @@ IF EXIST .\build rmdir /s /q .\build
 
 IF NOT EXIST .\build MKDIR .\build
 
-ECHO STEP 2) Running CMAKE...
-set OpenSSLDir=%INSTALLDIR%\openssl
+ECHO STEP 1) Running CMAKE...
+set OPENSSL_ROOT_DIR=%INSTALLDIR%\..\..\third-party\openssl
 cd .\build
 %CMAKEEXE% ..
 
-ECHO STEP 3) Building project...
+ECHO STEP 2) Building project...
 msbuild ALL_BUILD.vcxproj /p:Configuration=Debug 
 
 ECHO STEP 4) Install Samples...
@@ -32,16 +32,10 @@ cd ..
 IF NOT EXIST %INSTALLDIR% MKDIR %INSTALLDIR%
 IF NOT EXIST %INSTALLDIR%\bin MKDIR %INSTALLDIR%\bin
 
-XCOPY /Y /Q ".\build\amqp_ansic_publisher\Debug\*.*" "%INSTALLDIR%\bin"
-XCOPY /Y /Q ".\build\amqp_ansic_subscriber\Debug\*.*" "%INSTALLDIR%\bin"
+XCOPY /Y /Q /I ".\build\oauth2_client\Debug\*.*" "%INSTALLDIR%\bin"
+XCOPY /Y /Q /S /I ".\build\oauth2_client\PKI" "%INSTALLDIR%\bin\PKI"
 
 ECHO *** ALL DONE ***
-GOTO theEnd
-
-:noSource
-ECHO.
-ECHO Azure-uAMQP-C source not found. Please check the path.
-ECHO Searched for: %SRCDIR%
 GOTO theEnd
 
 :theEnd
