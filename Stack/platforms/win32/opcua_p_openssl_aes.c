13b168a0952553068502aac2236fc3d8182752e2
Bernd Edlinger <bernd.edlinger@softing.com>
2017-03-08 12:48:18 +0100

    - fixed error handling of AES_CBC_Encrypt/Decrypt

--- Stack/platforms/win32/opcua_p_openssl_aes.c
+++ Stack/platforms/win32/opcua_p_openssl_aes.c
@@ -43,8 +43,7 @@ OpcUa_StatusCode OpcUa_P_OpenSSL_AES_CBC_Encrypt(
 {
     AES_KEY         key;
 
-    OpcUa_Byte*     pInitalVector = OpcUa_Null;
-    OpcUa_UInt32    keyLenInBits  = 0;
+    OpcUa_Byte      pInitalVector[16];
 
     OpcUa_InitializeStatus(OpcUa_Module_P_OpenSSL, "AES_CBC_Encrypt");
 
@@ -56,18 +55,9 @@ OpcUa_StatusCode OpcUa_P_OpenSSL_AES_CBC_Encrypt(
     OpcUa_ReturnErrorIfArgumentNull(a_pInitalVector);
     OpcUa_ReturnErrorIfArgumentNull(a_pCipherTextLen);
 
-    if((((OpcUa_Int)a_plainTextLen) < 0) || (a_key->Key.Length < 0))
+    if(a_plainTextLen % 16 != 0)
     {
         uStatus = OpcUa_BadInvalidArgument;
-        OpcUa_GotoErrorIfBad(uStatus);
-    }
-
-    keyLenInBits = (a_key->Key.Length*8);
-
-    /* we have to pass the key length in bits instead of bytes */
-    if(AES_set_encrypt_key(a_key->Key.Data, keyLenInBits, &key) < 0)
-    {
-        uStatus = OpcUa_Bad;
         OpcUa_GotoErrorIfBad(uStatus);
     }
 
@@ -79,29 +69,26 @@ OpcUa_StatusCode OpcUa_P_OpenSSL_AES_CBC_Encrypt(
         OpcUa_ReturnStatusCode;
     }
 
+    /* we have to pass the key length in bits instead of bytes */
+    if(AES_set_encrypt_key(a_key->Key.Data, a_key->Key.Length * 8, &key) < 0)
+    {
+        uStatus = OpcUa_Bad;
+        OpcUa_GotoErrorIfBad(uStatus);
+    }
+
     /* copy the IV because the AES_cbc_encrypt function overwrites it. */
-    pInitalVector = (OpcUa_Byte*)OpcUa_P_Memory_Alloc(16);
-    OpcUa_ReturnErrorIfAllocFailed(pInitalVector);
     OpcUa_P_Memory_MemCpy(pInitalVector, 16, a_pInitalVector, 16);
 
     /* encrypt data */
     AES_cbc_encrypt(    a_pPlainText,
                         a_pCipherText,
-                        *a_pCipherTextLen,
+                        a_plainTextLen,
                         &key,
                         pInitalVector,
                         AES_ENCRYPT);
 
-    OpcUa_ReturnErrorIfNull(a_pCipherText, OpcUa_Bad);
-
-    OpcUa_P_Memory_Free(pInitalVector);
-    pInitalVector = OpcUa_Null;
-
 OpcUa_ReturnStatusCode;
 OpcUa_BeginErrorHandling;
-
-    OpcUa_P_Memory_Free(pInitalVector);
-    pInitalVector = OpcUa_Null;
 
 OpcUa_FinishErrorHandling;
 }
@@ -119,7 +106,7 @@ OpcUa_StatusCode OpcUa_P_OpenSSL_AES_CBC_Decrypt(
     OpcUa_UInt32*           a_pPlainTextLen)
 {
     AES_KEY         key;
-    OpcUa_Byte*     pInitalVector = OpcUa_Null;
+    OpcUa_Byte      pInitalVector[16];
 
     OpcUa_InitializeStatus(OpcUa_Module_P_OpenSSL, "AES_CBC_Decrypt");
 
@@ -130,21 +117,27 @@ OpcUa_StatusCode OpcUa_P_OpenSSL_AES_CBC_Decrypt(
     OpcUa_ReturnErrorIfArgumentNull(a_key->Key.Data);
     OpcUa_ReturnErrorIfArgumentNull(a_pInitalVector);
 
+    if(a_cipherTextLen % 16 != 0)
+    {
+        uStatus = OpcUa_BadInvalidArgument;
+        OpcUa_GotoErrorIfBad(uStatus);
+    }
+
+    *a_pPlainTextLen = a_cipherTextLen;
+
+    /* if just the output length is needed for the caller of this function */
     if(a_pPlainText == OpcUa_Null)
     {
-        *a_pPlainTextLen = a_cipherTextLen;
         OpcUa_ReturnStatusCode;
     }
 
     /* we have to pass the key length in bits instead of bytes */
-    if(AES_set_decrypt_key(a_key->Key.Data,(a_key->Key.Length*8),&key) < 0)
+    if(AES_set_decrypt_key(a_key->Key.Data, a_key->Key.Length * 8, &key) < 0)
     {
         OpcUa_GotoErrorWithStatus(OpcUa_Bad);
     }
 
     /* copy the IV because the AES_cbc_encrypt function overwrites it. */
-    pInitalVector = (OpcUa_Byte*)OpcUa_P_Memory_Alloc(16);
-    OpcUa_ReturnErrorIfAllocFailed(pInitalVector);
     OpcUa_P_Memory_MemCpy(pInitalVector, 16, a_pInitalVector, 16);
 
     /* decrypt ciphertext */
@@ -155,17 +148,8 @@ OpcUa_StatusCode OpcUa_P_OpenSSL_AES_CBC_Decrypt(
                         pInitalVector,
                         AES_DECRYPT);
 
-    /* get real message length */
-    *a_pPlainTextLen = a_cipherTextLen;
-
-    OpcUa_P_Memory_Free(pInitalVector);
-    pInitalVector = OpcUa_Null;
-
 OpcUa_ReturnStatusCode;
 OpcUa_BeginErrorHandling;
-
-    OpcUa_P_Memory_Free(pInitalVector);
-    pInitalVector = OpcUa_Null;
 
 OpcUa_FinishErrorHandling;
 }
